aef35c2c5176c125358aacc004d30c0c0521bce7
aleusa <57847350+aleusa@users.noreply.github.com>
2023-06-21 21:30:44 -0400

    Starting Design

--- src/Kernel/Pages/HomePage/HomePage.js
+++ src/Kernel/Pages/HomePage/HomePage.js
@@ -4,12 +4,34 @@ import { Images } from "../../../assets/images/images";
 export const HomePage = () => {
   return (
     <div className="HomePage">
-      <div className="HomePageHeader">
-        <h1>Frontend Engineer</h1>
-        <img className="HomePageSelfPortrait" src={Images.SelfPortrait} />
-        <img className="HomePageCodingImage" src={Images.Coding} />
+      <div className="HomePageIntroSection">
+        <div className="HomePageHeader">
+          <div className="HomePageHeaderName">jose_jon</div>
+          <div className="HomePageHeaderButtons">
+            <button className="ResumeButton">Resume</button>
+            <button className="SayHelloButton">Say Hello</button>
+          </div>
+        </div>
+        <div className="HomePageIntroduction">
+          <div className="HomePageIntroductionLeftSide">
+            <div className="HomePageIntroductionTitle">
+              Software Engineer with a Passion for Innovation
+            </div>
+            <div className="HomePageIntroductionLineBreak" />
+            <div className="HomePageIntroductionDescription">
+              With a diverse skill set and a commitment to delivering
+              high-quality products, I am dedicated to providing software
+              solutions that exceed the expectations of my clients.
+            </div>
+          </div>
+          <div className="HomePageIntroductionRightSide">
+            <img
+              className="HomePageBackgroundImage"
+              src={Images.IntroBackground}
+            />
+          </div>
+        </div>
       </div>
-      <div className="HomePageDescription"></div>
     </div>
   );
 };
--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,7 +1,9 @@
 import selfPortrait from "./resources/self-portrait.jpg";
 import coding from "./resources/coding.png";
+import introBackground from "./resources/IntroBackground.png";
 
 export const Images = {
+  IntroBackground: introBackground,
   Coding: coding,
   SelfPortrait: selfPortrait,
 };
